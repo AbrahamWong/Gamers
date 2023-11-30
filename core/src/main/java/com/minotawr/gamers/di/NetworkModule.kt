14340d1b4a7acac8904ee94a9d590a347ee0127f
Abraham Wong <abraham.wong@sg-dsa.com>
2023-11-30 10:20:39 +0700

    Add certificate pinner

--- core/src/main/java/com/minotawr/gamers/di/NetworkModule.kt
+++ core/src/main/java/com/minotawr/gamers/di/NetworkModule.kt
@@ -2,6 +2,7 @@ package com.minotawr.gamers.di
 
 import com.minotawr.gamers.core.BuildConfig
 import com.minotawr.gamers.data.remote.network.ApiService
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
@@ -24,12 +25,20 @@ fun provideConverterFactory() = GsonConverterFactory.create()
 fun provideHttpLoggingInterceptor() = HttpLoggingInterceptor()
     .setLevel(HttpLoggingInterceptor.Level.BODY)
 
-fun provideOkHttpClient(interceptor: HttpLoggingInterceptor) =
-    OkHttpClient.Builder()
+fun provideOkHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient {
+    val hostname = BuildConfig.HOST_NAME
+    val certificatePinner = CertificatePinner.Builder()
+        .add(hostname, "sha256/o/TPHqfOxqUVvhHmuaef0sC3tHur5b1L3XU/fDFwHJQ=")
+        .add(hostname, "sha256/81Wf12bcLlFHQAfJluxnzZ6Frg+oJ9PWY/Wrwur8viQ=")
+        .build()
+
+    return OkHttpClient.Builder()
         .connectTimeout(5, TimeUnit.SECONDS)
         .readTimeout(5, TimeUnit.SECONDS)
         .addInterceptor(interceptor)
+        .certificatePinner(certificatePinner)
         .build()
+}
 
 fun provideRetrofit(converterFactory: GsonConverterFactory, client: OkHttpClient) =
     Retrofit.Builder()
